1d787d6df1f41a0c8f4fa17d3292960e332d3e63
denniszhuge <denniszhuge@zego.im>
2021-11-22 19:35:37 +0800

    媒体播放器 playIndex 增加默认值，参数可选

--- lib/src/zego_mediaplayer.dart
+++ lib/src/zego_mediaplayer.dart
@@ -15,8 +15,8 @@ class ZegoMediaplayer {
   ///@param path 文件路径
   ///@param isRepeat 是否循环播放
   ///@param isAsset 是否为 flutter 的 asset 资源
-  static Future<void> start(String path,int playerIndex,
-      {bool isRepeat = false, bool isAsset = true}) async {
+  static Future<void> start(String path,
+      {int playerIndex = 0, bool isRepeat = false, bool isAsset = true}) async {
     return await _channel.invokeMethod(
         'mpStart', {'path': path, 'repeat': isRepeat, 'asset': isAsset,'playerIndex': playerIndex});
   }
@@ -29,7 +29,7 @@ class ZegoMediaplayer {
   ///@return textureID, flutter 渲染的纹理ID，非负数。当返回负数代表创建渲染器失败
   ///@discussion 当接口返回 TextureID 后，可通过 [Texture] 控件展示
   ///@discussion 当看到画面带有明显锯齿感时，请检查是否传入的宽高乘上了 devicePixelRatio, 参考[MediaQuery.of(context).devicePixelRatio]
-  static Future<int> createMediaPlayerRenderer(int width, int height,int playerIndex) async {
+  static Future<int> createMediaPlayerRenderer(int width, int height, {int playerIndex = 0}) async {
     final int textureID = await _channel.invokeMethod(
         'createMediaPlayerRenderer', {'width': width, 'height': height,'playerIndex':playerIndex});
 
@@ -43,7 +43,7 @@ class ZegoMediaplayer {
   ///@return true 成功，false 失败
   ///@discussion 当需要更新 Texture 渲染控件的大小时，调用本API同步更新渲染器的渲染大小，否则可能会导致图像变形等问题
   ///@discussion 当看到画面带有明显锯齿感时，请检查是否传入的宽高乘上了 devicePixelRatio, 参考[MediaQuery.of(context).devicePixelRatio]
-  static Future<void> updateMediaPlayRenderSize(int playerIndex,int width, int height) async {
+  static Future<void> updateMediaPlayRenderSize(int width, int height, {int playerIndex = 0}) async {
     return await _channel.invokeMethod(
         'updateMediaPlayRenderSize', {'width': width, 'height': height,'playerIndex': playerIndex});
   }
@@ -53,7 +53,7 @@ class ZegoMediaplayer {
   ///
   ///@return true 成功，false 失败
   ///@discussion 释放渲染器资源
-  static Future<bool> destroyMediaPlayerRenderer(int playerIndex) async {
+  static Future<bool> destroyMediaPlayerRenderer({int playerIndex = 0}) async {
     final bool success =
         await _channel.invokeMethod('destroyMediaPlayerRenderer',{'playerIndex': playerIndex});
 
@@ -64,7 +64,7 @@ class ZegoMediaplayer {
   ///
   ///@param View创建后的回调，viewID 为 Platform View 的唯一标识，请开发者自行管理
   ///@discussion 只有当 [ZegoLiveRoomPlugin.enablePlatformView] 传值为 true 时，调用该API有效，否则会返回错误
-  static Widget createMediaPlayerPlatformView(int playerIndex, Function(int viewID) onViewCreated) {
+  static Widget createMediaPlayerPlatformView(Function(int viewID) onViewCreated, {int playerIndex = 0}) {
     if(TargetPlatform.iOS == defaultTargetPlatform) {
 
       return UiKitView(
@@ -94,7 +94,7 @@ class ZegoMediaplayer {
   ///@param playerIndex 媒体播放器实例索引 ，该参数仅能传入实例索引
   ///@param viewID，调用 [createMediaPlayerPlatformView] 之后返回的 Platform View 的唯一标识，仅在使用 Platform View 渲染时才需要传入该参数
   ///@return 成功，false 失败
-  static Future<bool> destroyMediaplayerPlatformView(int playerIndex) async{
+  static Future<bool> destroyMediaplayerPlatformView({int playerIndex = 0}) async{
     final bool success =
         await _channel.invokeMethod('destroyMediaPlayerPlatformView',{'playerIndex': playerIndex});
 
@@ -106,7 +106,7 @@ class ZegoMediaplayer {
   ///@param playerIndex 媒体播放器实例索引 ，该参数仅能传入实例索引
   ///@param viewID，调用 [createMediaPlayerPlatformView] 之后返回的 Platform View 的唯一标识，仅在使用 Platform View 渲染时才需要传入该参数
   ///@return 成功，false 失败
-  static Future<bool> startRenderPlatformView(int playerIndex, {@required int viewID}) async {
+  static Future<bool> startRenderPlatformView({int playerIndex = 0, @required int viewID}) async {
     print('[Flutter-Dart] startPlayingStream, playerIndex: $playerIndex');
     final bool success = await _channel.invokeMethod('createMediaPlayerPlatformView', {
       'playerIndex': playerIndex,
@@ -119,13 +119,13 @@ class ZegoMediaplayer {
 
   ///停止播放媒体文件
   ///
-  static Future<void> stop(int playerIndex) async {
+  static Future<void> stop({int playerIndex = 0}) async {
     return await _channel.invokeMethod('mpStop',{'playerIndex': playerIndex});
   }
 
   ///暂停播放
   ///
-  static Future<void> pause(int playerIndex) async {
+  static Future<void> pause({int playerIndex = 0}) async {
     return await _channel.invokeMethod('mpPause',{'playerIndex': playerIndex});
   }
 
@@ -133,14 +133,14 @@ class ZegoMediaplayer {
   ///
   ///@param soundID 音效ID，标识当前播放的是哪个音频。取值为非负数
   ///@discussion 当开发者调用了 [pause] 暂停播放音效后，通过调用本接口进行恢复
-  static Future<void> resume(int playerIndex) async {
+  static Future<void> resume({int playerIndex = 0}) async {
     return await _channel.invokeMethod('mpResume',{'playerIndex': playerIndex});
   }
 
   ///预加载媒体文件
   ///
   ///@param path 本地路径，支持 flutter asset 资源路径和绝对路径
-  static Future<void> preload(String path,int playerIndex,{bool isAsset = true}) async {
+  static Future<void> preload(String path, {int playerIndex = 0, bool isAsset = true}) async {
     return await _channel
         .invokeMethod('mpLoad', {'path': path, 'asset': isAsset,'playerIndex': playerIndex});
   }
@@ -148,7 +148,7 @@ class ZegoMediaplayer {
   ///获取播放器当前状态
   ///
   ///@return true 正在播放，false 不在播放
-  static Future<bool> isPlaying(int playerIndex) async {
+  static Future<bool> isPlaying({int playerIndex = 0}) async {
     return await _channel.invokeMethod('mpIsPlaying',{'playerIndex': playerIndex});
   }
 
@@ -156,7 +156,7 @@ class ZegoMediaplayer {
   ///
   ///@param soundID 音效ID，标识当前播放的是哪个音频。取值为非负数
   ///@param volume 音量，取值范围为 0~100，默认为 50
-  static Future<void> setVolume(int volume,int playerIndex) async {
+  static Future<void> setVolume(int volume, {int playerIndex = 0}) async {
     return await _channel.invokeMethod('mpSetVolume', {'volume': volume,'playerIndex': playerIndex});
   }
 
@@ -164,7 +164,8 @@ class ZegoMediaplayer {
   ///
   ///@param soundID 音效ID，标识当前播放的是哪个音频。取值为非负数
   ///@param timestamp 需要跳转的进度，单位毫秒
-  static Future<ZegoMediaplayerSeekResult> seekTo(int timestamp,int playerIndex) async {
+  static Future<ZegoMediaplayerSeekResult> seekTo(int timestamp,
+      {int playerIndex = 0}) async {
     final Map<dynamic, dynamic> ret =
         await _channel.invokeMethod('mpSeekTo', {'timestamp': timestamp,'playerIndex': playerIndex});
 
@@ -178,7 +179,7 @@ class ZegoMediaplayer {
   ///
   ///@param soundID 音效ID，标识当前播放的是哪个音频。取值为非负数
   ///@return timestamp 总时长，单位毫秒；获取失败则返回 0
-  static Future<int> getTotalDuration(int playerIndex) async {
+  static Future<int> getTotalDuration({int playerIndex = 0}) async {
     final int timestamp = await _channel.invokeMethod('mpGetDuration',{'playerIndex': playerIndex});
 
     return timestamp;
@@ -188,7 +189,7 @@ class ZegoMediaplayer {
   ///
   ///@param soundID 音效ID，标识当前播放的是哪个音频。取值为非负数
   ///@return timestamp 当前播放进度，单位毫秒；获取失败则返回 0
-  static Future<int> getCurrentDuration(int playerIndex) async {
+  static Future<int> getCurrentDuration({int playerIndex = 0}) async {
     final int timestamp = await _channel.invokeMethod('mpGetCurrentDuration',{'playerIndex': playerIndex});
 
     return timestamp;
@@ -197,28 +198,30 @@ class ZegoMediaplayer {
   ///本地静默播放
   ///
   ///@param mute 是否需要静音本地播放
-  static Future<void> muteLocal(bool mute,int playerIndex) async {
+  static Future<void> muteLocal(bool mute, {int playerIndex = 0}) async {
     return await _channel.invokeMethod('mpMuteLocal', {'mute': mute,'playerIndex': playerIndex});
   }
 
   /// 设置播放类型
   ///
   /// @param type ，参考[]
-  static Future<void> setPlayerType(int type,int playerIndex) async {
+  static Future<void> setPlayerType(int type, {int playerIndex = 0}) async {
     return await _channel.invokeMethod('mpSetPlayerType', {'type': type,'playerIndex': playerIndex});
   }
 
-  static Future<void> setProcessInterval(int interval,int playerIndex) async {
+  static Future<void> setProcessInterval(int interval, {int playerIndex = 0}) async {
     return await _channel
         .invokeMethod('mpSetProcessInterval', {'interval': interval,'playerIndex': playerIndex});
   }
 
-  static Future<void> setOnlineResourceCache(int time, int size,int playerIndex) async {
+  static Future<void> setOnlineResourceCache(int time, int size,
+      {int playerIndex = 0}) async {
     return await _channel
         .invokeMethod('setOnlineResourceCache', {'time': time, 'size': size,'playerIndex': playerIndex});
   }
 
-  static Future<ZegoMediaplayerCacheResult> getOnlineResourceCache(int playerIndex) async {
+  static Future<ZegoMediaplayerCacheResult> getOnlineResourceCache(
+      {int playerIndex = 0}) async {
     final Map<dynamic, dynamic> map =
         await _channel.invokeMethod('getOnlineResourceCache',{'playerIndex': playerIndex});
 
@@ -227,12 +230,12 @@ class ZegoMediaplayer {
     return result;
   }
 
-  static Future<void> setBufferThreshold(int threshold,int playerIndex) async {
+  static Future<void> setBufferThreshold(int threshold, {int playerIndex = 0}) async {
     return await _channel
         .invokeMethod('setBufferThreshold', {'threshold': threshold,'playerIndex': playerIndex});
   }
 
-  static Future<void> setLoadResourceTimeout(int timeout,int playerIndex) async {
+  static Future<void> setLoadResourceTimeout(int timeout, {int playerIndex = 0}) async {
     return await _channel
         .invokeMethod('setLoadResourceTimeout', {'timeout': timeout,'playerIndex': playerIndex});
   }
@@ -240,7 +243,7 @@ class ZegoMediaplayer {
   ///设置播放文件的音轨
   ///
   ///@param streamIndex 音轨序号，可以通过 getAudioStreamCount 接口获取音轨个数
-  static Future<void> setAudioStream(int streamIndex,int playerIndex) async {
+  static Future<void> setAudioStream(int streamIndex, {int playerIndex = 0}) async {
     return await _channel
         .invokeMethod('mpSetAudioStream', {'streamIndex': streamIndex,'playerIndex': playerIndex});
   }
@@ -248,7 +251,7 @@ class ZegoMediaplayer {
   ///获取音轨个数
   ///
   ///@return 音轨个数
-  static Future<int> getAudioStreamCount(int playerIndex) async {
+  static Future<int> getAudioStreamCount({int playerIndex = 0}) async {
     int count = await _channel.invokeMethod('mpGetAudioStreamCount',{'playerIndex': playerIndex});
     return count;
   }
@@ -256,7 +259,7 @@ class ZegoMediaplayer {
   ///获取推流音量
   ///
   ///@return 推流音量
-  static Future<int> getPublishVolume(int playerIndex) async {
+  static Future<int> getPublishVolume({int playerIndex = 0}) async {
     int count = await _channel.invokeMethod('mpGetPublishVolume',{'playerIndex': playerIndex});
     return count;
   }
@@ -264,7 +267,7 @@ class ZegoMediaplayer {
   ///获取本地播放音量
   ///
   ///@return 本地播放音量
-  static Future<int> getPlayVolume(int playerIndex) async {
+  static Future<int> getPlayVolume({int playerIndex = 0}) async {
     int count = await _channel.invokeMethod('mpGetPlayVolume',{'playerIndex': playerIndex});
     return count;
   }
@@ -272,7 +275,7 @@ class ZegoMediaplayer {
   ///设置推流音量
   ///
   ///@param volume 音量，取值范围[0, 200]，默认 60
-  static Future<void> setPublishVolume(int volume,int playerIndex) async {
+  static Future<void> setPublishVolume(int volume, {int playerIndex = 0}) async {
     return await _channel
         .invokeMethod('mpSetPublishVolume', {'volume': volume,'playerIndex': playerIndex});
   }
@@ -280,7 +283,7 @@ class ZegoMediaplayer {
   ///设置本地播放音量
   ///
   ///@param volume 音量，取值范围[0, 200]，默认 60
-  static Future<void> setPlayVolume(int volume,int playerIndex) async {
+  static Future<void> setPlayVolume(int volume, {int playerIndex = 0}) async {
     return await _channel.invokeMethod('mpSetPlayVolume', {'volume': volume,'playerIndex': playerIndex});
   }
 
